6e5c95747922035eaae470a9434b2c3c55812120
jhonnyberdeja <jhonny.berdeja.100@gmail.com>
2016-01-23 15:02:06 -0300

    Agrgamos MainactivityGPS con land por defecto

--- MyApplicationRama/app/src/main/java/com/example/jhonny/myapplicationrama/MainActivityGPS.java
+++ MyApplicationRama/app/src/main/java/com/example/jhonny/myapplicationrama/MainActivityGPS.java
@@ -1,36 +1,24 @@
 package com.example.jhonny.myapplicationrama;
 
-
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 
-public class MainActivityDeInicio extends ActionBarActivity {
+public class MainActivityGPS extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_activity_de_inicio);
-        Button botonGPS=(Button)findViewById(R.id.idBotonGPS);
-        botonGPS.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(getApplicationContext(), MainActivityGPS.class);
-                startActivity(intent);
-            }
-        });
+        setContentView(R.layout.activity_main_activity_gps);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main_activity_de_inicio, menu);
+        getMenuInflater().inflate(R.menu.menu_main_activity_g, menu);
         return true;
     }
 

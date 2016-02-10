1618cda72c6c0cd77e45f42ce129ace7239e3d5c
jhonny berdeja <jhonny.berdeja.100@gmail.com>
2016-02-10 00:32:17 -0300

    agragamos la funcioncionalidad de mostrar el mapa

--- MyApplicationRama/app/src/main/java/com/example/jhonny/myapplicationrama/MainActivityMostrarMapa.java
+++ MyApplicationRama/app/src/main/java/com/example/jhonny/myapplicationrama/MainActivityMostrarMapa.java
@@ -1,17 +1,62 @@
 package com.example.jhonny.myapplicationrama;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+
 
 public class MainActivityMostrarMapa extends ActionBarActivity {
+
+    GoogleMap googleMap;
+    MapView mapView;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activity_mostrar_mapa);
+        mapView = (MapView) findViewById(R.id.id_de_mapa);
+        mapView.onCreate(savedInstanceState);
+        googleMap = mapView.getMap();
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        googleMap.setMyLocationEnabled(true);
+
     }
 
 
